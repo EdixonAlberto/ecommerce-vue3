de467366e543be461b251b31bfeedf50479b590a
Edixon Piña <edixonalbertto@gmail.com>
2021-04-11 22:55:58 -0400

    adding section in view home

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="home">
     <div class="banner"></div>
+
+    <section>
+      <div class="eco-container">
+        <h2>Lorem ipsum dolor sit amet</h2>
+
+        <div class="steps">
+          <div v-for="(item, i) of [1, 2, 3]" :key="i" class="card-step">
+            <div class="icon">icon</div>
+            <h4>Title</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem
+              corrupti expedita maxime beatae alias cupiditate labore reiciendis a non
+              libero.
+            </p>
+            <router-link to="#">Read More</router-link>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
